00a9fe46448d568670e73f228269ada919035144
HuJAY <hujiayu005@gmail.com>
2026-05-02 20:21:14 +0800

    Initialize OLED and Motor with throttle control

--- Demos/demo-v1.0/main.c
+++ Demos/demo-v1.0/main.c
@@ -1,1 +1,24 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "throttle.h"
+#include "Motor.h"
+
+uint16_t ThrottleVal;
+
+int main(void)
+{
+	OLED_Init();
+	Motor_Init();
+	Throttle_ADC_Init();
+	
+	OLED_ShowString(1,1,"throttle:000%");
+	
+	while(1)
+	{
+		ThrottleVal=Get_ThrottleValue();
+			OLED_ShowNum(1,10,ThrottleVal,3);
+			Motor_SetSpeed(ThrottleVal);
+	}
+}
 
